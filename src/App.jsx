d47e45df0d06425f58937282ee4cd50786216e36
vadym <hryshchenko@gmail.com>
2022-09-28 23:16:57 +0300

    added GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 // import { useEffect } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+
 import './App.css';
+import Header from './components/Header';
 // import { getProducts } from './redux/slices/productsSlice';
+
+const GlobalStyle = createGlobalStyle`
+*,*::before,*::after {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+a{
+  text-decoration: none;
+}
+ul,ol,li{
+  list-style: none;
+}
+h1,h2,h3{
+  font-weight: inherit;
+  font-size: inherit;
+}
+html,body{
+  font-size: 16px;
+  color: black;
+  font-family: 'Poppins', sans-serif;
+}
+`;
 
 function App() {
   // const dispatch = useDispatch()
@@ -11,11 +38,10 @@ function App() {
   //   dispatch(getProducts())
   // }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        header
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
 
