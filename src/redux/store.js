678981cc34eb1505c6002b991a1aca973791f8cb
vadym <hryshchenko@gmail.com>
2022-09-29 17:48:42 +0300

    created store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import productSlice from './slices/productSlice'
 import productsSlice from './slices/productsSlice'
 
 export const store = configureStore({
   reducer: {
     products: productsSlice,
+    product: productSlice,
   },
 })
